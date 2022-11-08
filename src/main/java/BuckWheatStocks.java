c575962b2de4f6d17bb118e0b5e7a1e52c281820
Ilya Danilov <esozap32@mail.ru>
2022-11-08 23:05:48 +0400

    BuckWheatStocks

--- src/main/java/BuckWheatStocks.java
+++ src/main/java/BuckWheatStocks.java
@@ -1,10 +1,26 @@
 public class BuckWheatStocks {
     public static void main(String[] args) {
+int sum = 1;
+        int stock = 100; //Кг
+        int price = 100; //Цена за кг
+        int onemonth = 6; // Съедает в месяц кг
+        int month = 14; // Сколько месяцев планирует питаться Василий
+        int payment = price;
 
-        int stocks = 100;
-        int storageOfOneKilogram = 100;
-        int eatsInOneMonth = 6;
-        int payment = storageOfOneKilogram;
+        do {
+            stock = stock - onemonth;
+            System.out.println();
+           // stock--;
 
-        }
+            payment = (stock * price);
+            System.out.println(payment + " рублей ");
+            month--;
+
+            sum = (stock * payment) ;
+
+
+        } while (month > 0);
+        System.out.println("Сумма которую должен заплатить василий за 14 месяцев = "
+                + sum);
     }
+}
